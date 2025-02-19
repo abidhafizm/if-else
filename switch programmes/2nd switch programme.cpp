27bb219559c88c9aec17f5bb75fe53cb683ad2dd
Muhammad Abid <mabid.ggs20@gmail.com>
2025-02-19 09:43:25 +0500

    Add files via upload

---
+++ switch programmes/2nd switch programme.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+int main ()
+{ 
+ int num; 
+ cout<<"Enter a number:"; 
+ cin>>num; 
+ switch (num) 
+ { 
+ case 20: 
+ cout<<"It is 20"; 
+ break; 
+ case 30: 
+ cout<<"It is 30"; 
+ break; 
+ case 40: 
+ cout<<"It is 40"; 
+ break; 
+ default: 
+ cout<<"Not 20, 30 or 40"; 
+ } 
+ return 0;
+} 
